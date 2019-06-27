5a60955a721a88b4318eee848493c67097dce4f7
Gui Priaulx <8568379+gpriaulx@users.noreply.github.com>
2019-06-27 20:57:39 +1000

    Initial mockup

---
+++ BlazorTestx/BlazorTestx/Pages/Counter.razor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace BlazorTestx.Pages
+{
+    public class ViewForModel<T> : ComponentBase where T : ReactiveObject
+    {
+        private IDisposable viewModelSubscriptions;
+        private T viewModel;
+
+        public T ViewModel
+        {
+            get => viewModel;
+            set
+            {
+                viewModelSubscriptions?.Dispose();
+
+                viewModel = value;
+
+                viewModelSubscriptions = value.Changed.Do(_ => StateHasChanged()).Subscribe();
+            }
+        }
+    }
+
+    public class CounterViewModel : ReactiveObject
+    {
+        private readonly IDisposable disposables;
+
+        [Reactive]
+        public int Counter { get; set; }
+
+        public CounterViewModel()
+        {
+            disposables = Observable.Timer(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2)).ObserveOn(CurrentThreadScheduler.Instance).Do(x =>
+            {
+                Counter++;
+            }).Subscribe();
+        }
+    }
+}
+
+
+//disposables = Observable.Timer(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2)).Do(x =>
+//{
+//Invoke(() =>
+//{
+//    currentCount++;
+//    StateHasChanged();
+//});
+//}).Subscribe();
