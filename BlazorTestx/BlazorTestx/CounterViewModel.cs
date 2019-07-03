beb426e18c0b66bd9a64630c451206f0e29522a5
Gui Priaulx <8568379+gpriaulx@users.noreply.github.com>
2019-07-03 13:29:39 +1000

    Use types provided by base

---
+++ BlazorTestx/BlazorTestx/CounterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace BlazorTestx
+{
+    public class CounterViewModel : ReactiveObject
+    {
+
+        private readonly IDisposable disposables;
+
+        [Reactive]
+        public int Counter { get; set; }
+
+        public CounterViewModel()
+        {
+            disposables = Observable.Timer(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2))
+                .Do(x =>
+                {
+                    Counter++;
+                }).Subscribe();
+        }
+    }
+}
